d1c8b3e3e2943496fe1cfa3250b7f7f2b2090579
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-02-16 12:16:25 -0300

    chore: iniciação de diversas aplicações

--- brawler/models.py
+++ brawler/models.py
@@ -1,3 +1,23 @@
 from django.db import models
+# from proficiencies.models import Proficiency
 
 # Create your models here.
+class Brawler(models.Model):
+    brawler_id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=25, verbose_name="Brawler Name")
+    primary_class = models.CharField(max_length=2, choices=[('D', 'Damage Dealer'), ('T', 'Tank'), ('AS', 'Assasin'), ('S', 'Support'), ('C', 'Controler'), ('M', 'Marksman'), ('AT', 'Artillery')], verbose_name="Primary Class")
+    secondary_class = models.CharField(max_length=2, choices=[('D', 'Damage Dealer'), ('T', 'Tank'), ('AS', 'Assasin'), ('S', 'Support'), ('C', 'Controler'), ('M', 'Marksman'), ('AT', 'Artillery')], verbose_name="Secondary Class")
+    # base_proficiency = models.ForeignKey(Proficiency, on_delete=models.PROTECT, verbose_name="Base Proficiency", default=)
+    # first_gadget
+    # second_gadget
+    # first_star_power
+    # second_star_power
+    # hipercharge
+
+    class Meta:
+        verbose_name = "Brawler"
+        verbose_name_plural = "Brawlers"
+        ordering = ['primary_class', 'secondary_class']
+    
+    def __str__(self):
+        return self.name
---
+++ proficiency/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ProficiencyConfig(AppConfig):
+    name = 'proficiency'
