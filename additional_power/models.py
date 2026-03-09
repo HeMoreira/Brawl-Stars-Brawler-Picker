faf30f406081fc4faf76ed9d885a68e8a997cf17
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-03-09 18:15:38 -0300

    fix: Modelos Gadget, StarPower e Hipercharge / Restauração do banco de dados
    1. Agora os modelos de Gadget, StarPower e Hipercharge também utilizam verificadores para o icon_name, além de herdarem uma mesma classe para evitar repetição.
    
    2. Nos últimos 2 commits, o banco de dados esteve com problemas relacionados a impossibilidade de acessar alguns objetos Proficiency. Isso aconteceu pois, durante testes, algumas dessas instâncias foram associadas à objetos de Map que foram, pouco tempo depois, excluídos. Devido a relação de pais com seus filhos Proficiency (ForeignKey e PROTECT), essa interação acabou por ser corrompida. Isso foi corrigido ao restaurar o banco de dados nesse commit, mas a correção definitiva para este problema virá apenas em um commit futuro.

--- additional_power/models.py
+++ additional_power/models.py
@@ -1,36 +1,49 @@
 from django.db import models
 from proficiency.models import Proficiency
+from django.core.exceptions import ValidationError
 
-# Create your models here.
-class Gadget(models.Model):
+class AdditionalPower(models.Model):
     name = models.CharField(max_length=40)
+    icon_name = models.CharField(max_length=49)
     base_proficiency = models.ForeignKey(Proficiency, on_delete=models.PROTECT, verbose_name="Base Proficiency")
+
+    class Meta:
+        abstract = True
+    
+    def __str__(self):
+        return self.name
+    
+    def clean(self):
+        super().clean()
+        adapted_name = self.name.lower().replace(" ", "")
+        name_length = len(adapted_name)
+        if adapted_name != self.icon_name[:name_length] or self.icon_name[name_length:] != "_icon.png":
+            raise ValidationError({'icon_name': 'The icon_name needs to be composed of <brawlernameinlowercaseandwithoutspaces>_icon.png'})
+        
+    def save(self):
+        self.full_clean()
+        return super().save()
+
+class Gadget(AdditionalPower):
+    name = models.CharField(max_length=40, verbose_name="Gadget Name")
+    icon_name = models.CharField(max_length=49,  verbose_name="Gadget Icon Name", default="unowngadget_icon.png")
 
     class Meta:
         verbose_name = "Gadget"
         verbose_name_plural = "Gadgets"
-    
-    def __str__(self):
-        return self.name
 
-class StarPower(models.Model):
-    name = models.CharField(max_length=40)
-    base_proficiency = models.ForeignKey(Proficiency, on_delete=models.PROTECT, verbose_name="Base Proficiency")
+class StarPower(AdditionalPower):
+    name = models.CharField(max_length=40, verbose_name="Star Power Name")
+    icon_name = models.CharField(max_length=49,  verbose_name="Star Power Icon Name", default="unownstarpower_icon.png")
 
     class Meta:
         verbose_name = "Star Power"
         verbose_name_plural = "Star Powers"
-    
-    def __str__(self):
-        return self.name
 
-class Hipercharge(models.Model):
-    name = models.CharField(max_length=40)
-    base_proficiency = models.ForeignKey(Proficiency, on_delete=models.PROTECT, verbose_name="Base Proficiency")
+class Hipercharge(AdditionalPower):
+    name = models.CharField(max_length=40, verbose_name="Hipercharge Name")
+    icon_name = models.CharField(max_length=49,  verbose_name="Hypercharge Icon Name", default="unownhipercharge_icon.png")
 
     class Meta:
         verbose_name = "Hipercharge"
         verbose_name_plural = "Hipercharges"
-    
-    def __str__(self):
-        return self.name
