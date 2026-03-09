137fac327daefba1b09f9ac2722d01223d1ecde5
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-03-09 16:58:30 -0300

    feat: testes e adições para o modelo Map
    O modelo Map agora impede que um mapa com um icon_name inválido seja salvo. Além disso, testes que checam o funcionamento correto dessa funcionalidade também foram adicionados.

--- map/models.py
+++ map/models.py
@@ -1,9 +1,28 @@
 from django.db import models
 from proficiency.models import Proficiency
+from django.core.exceptions import ValidationError
 
 # Create your models here.
 class Map(models.Model):
+    class Meta:
+        verbose_name = "Map"
+        verbose_name_plural = "Maps"
+
     name = models.CharField(max_length=20, verbose_name="Name of the Map")
     icon_name = models.CharField(max_length=29, verbose_name="Icon of the Map", default="unownmap_icon.png")
     mode = models.CharField(max_length=2, verbose_name="Gamemode of the Map", choices=[('GG', 'Gem-Grab'), ('BW', 'Brawl-Ball'), ('BO', 'Bounty'), ('HE', 'Heist'), ('HZ', 'Hot-Zone'), ('KN', 'Knockout'), ('WO', 'Wipeout'), ('BH', 'Brawl-Hockey'), ('SW', 'Spirit-Wars'), ('PP', 'Present-Plunder'), ('BK', 'Basket-Brawl'), ('VY', 'Volley-Brawl'), ('PT', 'Paint-Brawl'), ('SC', 'Soul-Collector'), ('BA', 'Brawl-Arena'), ('TR', 'Token-Run'), ('TH', 'Treasure-Hunt'), ('DG', 'Dodgebrawl'), ('SB', 'Save-Blast')])
     proficiency_multiplier = models.ForeignKey(Proficiency, on_delete=models.PROTECT, verbose_name="Proficiency Multiplier", help_text="The proficiencies of brawlers and others are multiplied from this map proficiencies, so use 10 by default and rarely use numbers above 25. All numbers here are divided by ten before being used as a multiplier")
+
+    def clean(self):
+        super().clean()
+        adapted_name = self.name.lower().replace(" ", "")
+        name_length = len(adapted_name)
+        if adapted_name != self.icon_name[:name_length] or self.icon_name[name_length:] != "_icon.png":
+            raise ValidationError({'icon_name': 'The icon_name needs to be composed of <brawlernameinlowercaseandwithoutspaces>_icon.png'})
+        
+    def __str__(self):
+        return self.name
+    
+    def save(self):
+        self.full_clean()
+        return super().save()
--- map/tests.py
+++ map/tests.py
@@ -1,3 +1,28 @@
 from django.test import TestCase
+from .models import Map
+from proficiency.models import Proficiency
+from django.core.exceptions import ValidationError
 
 # Create your tests here.
+class MapModelTest(TestCase):
+    def setUp(self):
+        self.map = Map(name="Super Test Map", icon_name="supertestmap_icon.png", mode="GG", proficiency_multiplier=Proficiency())
+    
+    def test_acceptable_icon_name_according_to_without_spaces_rule(self):
+        self.map.icon_name = "super-testmap_icon.png"
+        with self.assertRaises(ValidationError): self.map.clean()
+
+    def test_acceptable_icon_name_according_to_correct_end_rule(self):
+        self.map.icon_name = "supertestmap_ic0n.png"
+        with self.assertRaises(ValidationError): self.map.clean()
+
+    def test_acceptable_icon_name_according_to_exact_name_rule(self):
+        self.map.icon_name = "suprrtestmap_icon.png"
+        with self.assertRaises(ValidationError): self.map.clean()
+
+    def test_acceptable_icon_name(self):
+        self.map.icon_name = "supertestmap_icon.png"
+        try:
+            self.map.clean()
+        except(ValidationError):
+            self.fail("The name should be correct according to the name")
