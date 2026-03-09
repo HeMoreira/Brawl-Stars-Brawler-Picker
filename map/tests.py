dc605f0486134a72070ee83d177a1f47fbe33026
Henrique Moreira <henriquefmoreira2@gmail.com>
2026-03-09 17:10:55 -0300

    fix: ajuste de alguns testes de Map
    Agora, os testes abrangem melhor cada situação de ajuste com mais testes a partir de um nome bem diferente, o que aumenta a chance deles de denunciar mudanças que quebrem o funcionamento esperado.

--- map/tests.py
+++ map/tests.py
@@ -6,23 +6,32 @@ from django.core.exceptions import ValidationError
 # Create your tests here.
 class MapModelTest(TestCase):
     def setUp(self):
-        self.map = Map(name="Super Test Map", icon_name="supertestmap_icon.png", mode="GG", proficiency_multiplier=Proficiency())
+        self.map1 = Map(name="Super Test Map", icon_name="supertestmap_icon.png", mode="GG", proficiency_multiplier=Proficiency())
+        self.map2 = Map(name="Test Map With many Sp@c3s and stuff", icon_name="testmapwithmanysp@c3sandstuff_icon.png", mode="BB", proficiency_multiplier=Proficiency())
     
     def test_acceptable_icon_name_according_to_without_spaces_rule(self):
-        self.map.icon_name = "super-testmap_icon.png"
-        with self.assertRaises(ValidationError): self.map.clean()
+        self.map1.icon_name = "super-testmap_icon.png"
+        with self.assertRaises(ValidationError): self.map1.clean()
+        self.map2.icon_name = "testmap-withmany sp@c3sandstuff_icon.png"
+        with self.assertRaises(ValidationError): self.map2.clean()
 
     def test_acceptable_icon_name_according_to_correct_end_rule(self):
-        self.map.icon_name = "supertestmap_ic0n.png"
-        with self.assertRaises(ValidationError): self.map.clean()
+        self.map1.icon_name = "supertestmap_ic0n.png"
+        with self.assertRaises(ValidationError): self.map1.clean()
+        self.map2.icon_name = "testmapwithmanysp@c3sandstuff_iconn.png"
+        with self.assertRaises(ValidationError): self.map2.clean()
 
     def test_acceptable_icon_name_according_to_exact_name_rule(self):
-        self.map.icon_name = "suprrtestmap_icon.png"
-        with self.assertRaises(ValidationError): self.map.clean()
+        self.map1.icon_name = "suprrtestmap_icon.png"
+        with self.assertRaises(ValidationError): self.map1.clean()
+        self.map2.icon_name = "testmapwithmanyspac3sandstuff_icon.png"
+        with self.assertRaises(ValidationError): self.map2.clean()
 
     def test_acceptable_icon_name(self):
-        self.map.icon_name = "supertestmap_icon.png"
+        self.map1.icon_name = "supertestmap_icon.png"
+        self.map2.icon_name = "testmapwithmanysp@c3sandstuff_icon.png"
         try:
-            self.map.clean()
+            self.map1.clean()
+            self.map2.clean()
         except(ValidationError):
-            self.fail("The name should be correct according to the name")
+            self.fail("The names should be correct according to the name")
